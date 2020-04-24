f19b848dfa30111cf99d87f08ba156f4fca24996
Sebastian Łach <root@slach.eu>
2020-04-24 19:09:44 +0200

    Add test test_can_see_all_mail

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -1,4 +1,4 @@
-def test_can_see_empty_email_list(client):
+def test_can_see_empty_mail_list(client):
     rv = client.get('/api/mail')
     assert b'[]' in rv.data
 
@@ -11,6 +11,12 @@ def test_can_create_a_new_mail(client):
     assert b'"content"' in rv.data
     assert b'"created_at"' in rv.data
     assert b'"lorem ipsum"' in rv.data
+
+
+def test_can_see_all_mail(client):
+    rv = client.post('/api/mail', json={
+        'content': 'lorem ipsum',
+    })
 
     rv = client.get('/api/mail')
     assert b'"lorem ipsum"' in rv.data
