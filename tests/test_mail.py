c0a644aaf2da9ad2e76ac003086b1931c8611030
Sebastian Łach <root@slach.eu>
2020-04-25 13:42:09 +0200

    Add test test_can_define_one_recipient

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -37,3 +37,18 @@ def test_can_see_details_of_the_mail(client):
     assert 'id' in data
     assert 'created_at' in data
     assert data['content'] == 'lorem ipsum'
+
+
+def test_can_define_one_recipient(client):
+    rv = client.post('/api/mail', json={
+        'content': 'lorem ipsum',
+    })
+
+    rv = client.post('/api/mail/1/recipient', json={
+        'address': 'example@example.com',
+    })
+    data = json.loads(rv.data)
+    assert 'id' in data
+    assert 'name' in data
+    assert data['mail_id'] == 1
+    assert data['address'] == 'example@example.com'
